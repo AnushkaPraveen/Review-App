cda2f7e9eef3dd1ecee525573b258a9678adc2ac
AnushkaPraveen <anushkapraveen98@gmail.com>
2022-12-25 03:48:44 +0530

    home screen style chnages

--- components/homeScreen.js
+++ components/homeScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
-    View,Text,Button
+    View,Text,StyleSheet
 } from 'react-native';
+import { Button } from 'react-native-paper';
 
 const HomeScreen=({navigation})=>{
     return(
@@ -9,8 +10,25 @@ const HomeScreen=({navigation})=>{
             <Button title='See Price List' onPress={()=>navigation.navigate('Price')}/>
            <Button title='Create Review' onPress={()=>navigation.navigate('Create')}/>
             <Button title='See Reviews' onPress={()=>navigation.navigate('Review')}/>
+            <View>
+
+            </View>
+            <Button style={styles.button} mode="contained" onPress={()=>navigation.navigate('Price')}>
+    <Text style={styles.text}>Test</Text>
+  </Button>
             
         </View>
     )
 }
+const styles=StyleSheet.create({
+    button:{
+        backgroundColor:'#04144a',
+        padding:30,
+        margin:0,
+    
+    },
+    text:{
+        fontSize:20
+    }
+})
  export default HomeScreen;
